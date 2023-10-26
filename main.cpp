d5756da3ea2b1614ea478b809705e02aa9647bb6
GabrielWojcik251246 <251226@edu.p.lodz.pl>
2023-10-26 15:23:40 +0200

    Ostatecznie

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() {
-    std::cout << "Hello, World. Hello USA!" << std::endl;
+    std::cout << "Hello, World. Hello USA! 251246-moj nr" << std::endl;
     return 4;
 }
-/* Komentarz */
+/* Komentarz  Wójcik Gabriel- 251246 */
